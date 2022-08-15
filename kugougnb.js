4202b6a33098acb95035ce091c737ba64078d7ec
durianice <64821916+durianice@users.noreply.github.com>
2022-08-16 00:49:18 +0800

    Update kugougnb.js

--- kugougnb.js
+++ kugougnb.js
@@ -1,5 +1,7 @@
 var obj = JSON.parse($response.body);
        
-obj.data.follows=666;
+obj.data.fans = 96112400;
+obj.data.union_vipinfo.is_vip = 1;
+obj.data.svip_level = 6;
 
 $done({body : JSON.stringify(obj)});
