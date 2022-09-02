9429a5bd33418bfdc63ba8bc633f026b6db7d58d
durianice <64821916+durianice@users.noreply.github.com>
2022-09-02 18:57:40 +0800

    Update anyScript.js

--- anyScript.js
+++ anyScript.js
@@ -4,7 +4,7 @@ var url = $request.url;
 var match_0 = url.indexOf("nucleicAcid/v1/result") > -1
 
 if (match_0) {
-  obj.data[0].shift()
+  obj.data = [];
 //   obj.data[0].sampling_data = "2022-09-02 10:20:20";
 //   obj.data[0].testing_data = "2022-09-02 17:20:20";
 }
