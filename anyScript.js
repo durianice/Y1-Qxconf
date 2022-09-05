267822f4378cebbff39130664578a914b193ff6d
durianice <64821916+durianice@users.noreply.github.com>
2022-09-05 10:32:11 +0800

    Update anyScript.js

--- anyScript.js
+++ anyScript.js
@@ -17,16 +17,7 @@ if (match_0) {
   let endHms = `${getRandomIntInclusive(18, 23)}:${getRandomIntInclusive(0, 60)}:${getRandomIntInclusive(0, 60)}`;
   let sampling_date = `${yesterday} ${startHms}`;
   let testing_date = `${yesterday} ${endHms}`;
-  obj.data.unshift({
-      "xxzjbh" : "68fc9290a55345f7ace020c79f306a27",
-      "result" : "阴性",
-      "institution" : "广州白云云康达安医学检验实验室",
-      "identityType" : 1,
-      "sampling_date" : sampling_date,
-      "testing_date" : testing_date,
-      "identity" : "17b7187ed21b2fa84b269094e87de6ef0dff649101eebc583ca4b56e2dde0e2c",
-      "gj" : "中国",
-      "name" : "28bde6fd52d00e96529bed746589fd68"
-    });
+  let temp = { sampling_date, testing_date };
+  Object.assign(obj.data[0], temp);
 }
 $done({ body: JSON.stringify(obj) });
