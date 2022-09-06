d21a0bdc9f3dc44662d59e225bc071eecad52096
durianice <64821916+durianice@users.noreply.github.com>
2022-09-06 11:40:58 +0800

    Update anyScript.js

--- anyScript.js
+++ anyScript.js
@@ -11,18 +11,22 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function presetZero(num) {
+  if (typeof num !== 'string' || typeof num !== 'number') return '0';
+  num = Number(num);
+  return num > 9 ? num : '0' + num;
+}
 
 var match_0 = url.indexOf("nucleicAcid/v1/result") > -1;
 var match_1 = url.indexOf("api.iq.com/video/play") > -1;
 if (match_0) {
   let pastDate = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
   let y = pastDate.getFullYear();
-  let m = pastDate.getMonth() + 1;
-  m = m > 9 ? m : "0" + m;
-  let d = pastDate.getDate() > 9 ? pastDate.getDate() : '0' + pastDate.getDate();
+  let m = presetZero(pastDate.getMonth() + 1);
+  let d = presetZero(pastDate.getDate());
   let yesterday = `${y}-${m}-${d}`;
-  let startHms = `${getRandomIntInclusive(9, 12)}:${getRandomIntInclusive(0, 60)}:${getRandomIntInclusive(0, 60)}`;
-  let endHms = `${getRandomIntInclusive(18, 23)}:${getRandomIntInclusive(0, 60)}:${getRandomIntInclusive(0, 60)}`;
+  let startHms = `${presetZero(getRandomIntInclusive(9, 12))}:${presetZero(getRandomIntInclusive(0, 60))}:${presetZero(getRandomIntInclusive(0, 60))}`;
+  let endHms = `${presetZero(getRandomIntInclusive(18, 23))}:${presetZero(getRandomIntInclusive(0, 60))}:${presetZero(getRandomIntInclusive(0, 60))}`;
   let sampling_date = `${yesterday} ${startHms}`;
   let testing_date = `${yesterday} ${endHms}`;
   let temp = { sampling_date, testing_date };
