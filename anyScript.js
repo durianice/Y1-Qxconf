1f518b96f3ef50949b7e35b14416db6c224d8d48
durianice <64821916+durianice@users.noreply.github.com>
2022-10-06 18:16:09 +0800

    Update anyScript.js

--- anyScript.js
+++ anyScript.js
@@ -6,6 +6,7 @@
 
 var obj = JSON.parse($response.body);
 var url = $request.url;
+let util = int();
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -30,19 +31,36 @@ function getTestTime() {
   return { a: startDate, b: endDate };
 };
 
+function getTime() {
+  const exp_t = util.getdata("test_exp_t") || 0;
+  let result = null;
+  if (new Date().getTime() - Number(exp_t) > 24 * 60 * 60 * 1000) {
+    const t = getTestTime();
+    util.setdata("test_time", t);
+    util.setdata("test_exp_t", new Date().getTime());
+    result = t;
+  } else {
+    result = util.getdata("test_time");
+  };
+  return result;
+};
+
 var match_0 = url.indexOf("nucleicAcid/v1/result") > -1;
 var match_1 = url.indexOf("cat-match-static.easygame2021.com/maps") > -1;
 var match_2 = url.indexOf("nuclein/listNucleate") || url.indexOf("nuclein/listNucleateLast");
 if (match_0) {
-  let { a, b } = getTestTime();
+  let { a, b } = getTime();
   let temp = {
     sampling_date: a,
     testing_date: b
   };
-  Object.assign(obj.data[0], temp);
+  let last = JSON.parse(JSON.stringify(obj.data[0]));
+  Object.assign(last, temp);
+  obj.data.unshift(last);
+  // Object.assign(obj.data[0], temp);
 };
 if (match_2) {
-  let { a, b } = getTestTime();
+  let { a, b } = getTime();
   let temp = {
     sampleTime: a,
     nucleateCheckDate: b
@@ -52,190 +70,50 @@ if (match_2) {
   obj.data.unshift(last);
   // Object.assign(obj.data[0], temp);
 };
-if (match_1) {
-  const temp = {
-  "widthNum" : 8,
-  "blockTypeData" : {
-    "1" : 1,
-    "2" : 2,
-    "3" : 2
-  },
-  "levelKey" : 80001,
-  "heightNum" : 10,
-  "levelData" : {
-    "1" : [
-      {
-        "rolNum" : 16,
-        "rowNum" : 16,
-        "blockNode" : null,
-        "id" : "1-16-16",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 28,
-        "rowNum" : 16,
-        "blockNode" : null,
-        "id" : "1-28-16",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 40,
-        "rowNum" : 16,
-        "blockNode" : null,
-        "id" : "1-40-16",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 16,
-        "rowNum" : 32,
-        "blockNode" : null,
-        "id" : "1-16-32",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 28,
-        "rowNum" : 32,
-        "blockNode" : null,
-        "id" : "1-28-32",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 40,
-        "rowNum" : 32,
-        "blockNode" : null,
-        "id" : "1-40-32",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 16,
-        "rowNum" : 48,
-        "blockNode" : null,
-        "id" : "1-16-48",
-        "moldType" : 2,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 28,
-        "rowNum" : 48,
-        "blockNode" : null,
-        "id" : "1-28-48",
-        "moldType" : 2,
-        "type" : 0,
-        "layerNum" : 1
-      },
-      {
-        "rolNum" : 40,
-        "rowNum" : 48,
-        "blockNode" : null,
-        "id" : "1-40-48",
-        "moldType" : 2,
-        "type" : 0,
-        "layerNum" : 1
-      }
-    ],
-    "2" : [
-      {
-        "rolNum" : 16,
-        "rowNum" : 36,
-        "blockNode" : null,
-        "id" : "2-16-36",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 28,
-        "rowNum" : 36,
-        "blockNode" : null,
-        "id" : "2-28-36",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 40,
-        "rowNum" : 36,
-        "blockNode" : null,
-        "id" : "2-40-36",
-        "moldType" : 1,
-        "type" : 0,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 16,
-        "rowNum" : 20,
-        "blockNode" : null,
-        "id" : "2-16-20",
-        "moldType" : 1,
-        "type" : 1,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 28,
-        "rowNum" : 20,
-        "blockNode" : null,
-        "id" : "2-28-20",
-        "moldType" : 1,
-        "type" : 1,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 40,
-        "rowNum" : 20,
-        "blockNode" : null,
-        "id" : "2-40-20",
-        "moldType" : 1,
-        "type" : 1,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 16,
-        "rowNum" : 49,
-        "blockNode" : null,
-        "id" : "2-16-49",
-        "moldType" : 2,
-        "type" : 0,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 28,
-        "rowNum" : 49,
-        "blockNode" : null,
-        "id" : "2-28-49",
-        "moldType" : 2,
-        "type" : 0,
-        "layerNum" : 2
-      },
-      {
-        "rolNum" : 40,
-        "rowNum" : 49,
-        "blockNode" : null,
-        "id" : "2-40-49",
-        "moldType" : 2,
-        "type" : 0,
-        "layerNum" : 2
-      }
-    ],
-    "3" : [
-
-    ]
-  }
-  
-}
-  const data = JSON.parse(temp);
-  obj = data;
-}
 $done({ body: JSON.stringify(obj) });
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+chavy.done()
+
